4886bd9b31e3f1fd2df2cbf6a18d2f558c0233f7
Sergej Kukharev <sergej.kucharev@gmail.com>
2016-09-08 16:09:12 +0300

    1.0.7

--- test/index.js
+++ test/index.js
@@ -2,11 +2,16 @@ var pt = require('../');
 
 /**/
 var options = {
+	//proxy: 'http://200.9.220.120:3128',
+	//proxy: 'http://52.88.220.145:80',
+	//proxy: 'http://158.85.234.42:3128',
 	sl: 'ru',
 	tl: 'en',
+	uri: 'http://zanner.org.ua/t/test.html',
+	//uri: 'http://bash.im/',
 	//uri: 'http://myip.ru/index_small.php',
-	uri: 'http://t.zanner.org.ua/task-to-translate/57cb4ccafa9a00682b684fda',
-	fix: {link: 1, script: 1, iframe: 1, meta: 1, style: 1}
+	//uri: 'http://t.zanner.org.ua/task-to-translate/57cb4ccafa9a00682b684fda',
+	fix: true //{link: 1, script: 1, iframe: 1, meta: 1, style: 1}
 };
 pt.translate(options) // proxy:'http://104.236.222.191:3128'
 	.then(function(result){
@@ -17,7 +22,8 @@ pt.translate(options) // proxy:'http://104.236.222.191:3128'
 		//console.log(body);
 		//console.log(code);
 		//console.log(headers);
-		console.log($(':root').html());
+		console.log($('body').html());
+		//console.log($(':root').html());
 		//console.log($('table.network-info > tr > td').eq(0).html().trim());
 	}, function(error){
 		console.log(error);
